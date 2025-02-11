4ee82f0e5ef4fa5952d50173f334a22159629f68
Nikita Antonov <nikita45454@gmail.com>
2025-02-11 15:34:18 +0300

    feat: встраивает метрику

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { ViteEjsPlugin } from 'vite-plugin-ejs';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    ViteEjsPlugin({
+      title: 'Битва стилистов | ТРЦ «Планета» Красноярск',
+    }),
     tailwindcss(),
     vue(),
   ],
